ab1a59ed383977d76eeccbd774190d0004e6fece
GiorgiGoglidze <giorgigoglidze43@gmail.com>
2022-05-17 03:33:30 +0400

    aaaaa

---
+++ assets/index.js
@@ -0,0 +1,24 @@
+
+console.log("roger");
+console.log("I like roger federers playng style");
+console.log ("I love khinkali");
+
+
+//This is a coments
+
+/* This
+is 
+a
+multiline 
+comment
+*/
+
+
+let firstname="bro"
+console.log("hello", firstname)
+
+
+
+
+
+document.getElementById("p1").innerHTML="hello"+firstname;
