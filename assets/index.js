8673620e1e6678d110f041d0da7b94c2d618dda5
GiorgiGoglidze <giorgigoglidze43@gmail.com>
2022-05-28 01:16:46 +0400

    Update index.js

---
+++ assets/index.js
@@ -0,0 +1,18 @@
+function display (x,test)
+{
+    var message= test(x);
+    document.write(message);
+
+
+}
+function welcomeMessage (time)
+{
+    if (time<12);
+    return "dghgdj"
+    else if (time<18)
+    return "skghlshl"
+    else
+    return "gsdgkdlsh"
+}
+
+display (12,welcomeMessage);
